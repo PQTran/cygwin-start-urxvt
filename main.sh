e20dab294ef26f2437ce105953df4efb508ad3b8
Paul Tran <paul.tran@sap.com>
2018-05-15 13:46:09 -0700

    built on top of tmux for persistent processes

---
+++ main.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function start_xwin_session {
+    # start xwin server
+    xwin_session="session_xwin_server"
+    start_xwin_cmd="xwin -multiwindow -noclipboard"
+    tmux new-session -t $xwin_session -d
+    tmux send -t $xwin_session "$start_xwin_cmd"
+    tmux send -t $xwin_session C-m
+}
+
+function start_urxvt_session {
+    # start urxvt daemon
+    urxvtd_session="session_urxvt_daemon"
+    start_urxvtd_cmd="urxvtd"
+    tmux new-session -t $urxvtd_session -d
+    tmux send -t $urxvtd_session "$start_urxvtd_cmd"
+    tmux send -t $urxvtd_session C-m
+}
+
+function start_urxvt_client {
+    urxvtc -e tmux
+}
+
+function main {
+    if ! [[ $(pidof xwin) ]]; then
+        start_xwin_session
+    fi
+
+    if ! [[ $(pidof urxvtd) ]]; then
+        start_urxvt_session
+    fi
+
+    start_urxvt_client
+}
+
+main
